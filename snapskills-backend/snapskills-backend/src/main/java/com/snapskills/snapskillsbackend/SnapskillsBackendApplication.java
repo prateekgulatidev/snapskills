4df45f4ddc6b4ed54d2c7211f0c477571b4c219c
Prateek Gulati <prateekgulati333@gmail.com>
2025-07-05 20:47:41 +0530

    Backend spring initializer

---
+++ snapskills-backend/snapskills-backend/src/main/java/com/snapskills/snapskillsbackend/SnapskillsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snapskills.snapskillsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapskillsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapskillsBackendApplication.class, args);
+    }
+
+}
---
+++ snapskills-backend/snapskills-backend/src/test/java/com/snapskills/snapskillsbackend/SnapskillsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapskills.snapskillsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapskillsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
