f83b8761e530c7e8d751e28e8b3ae310d8dedd50
prateekgulatidev <prateekgulati333@gmail.com>
2025-07-06 05:49:14 +0530

    opendefination informaion and java upgrade to 24 in compose file

--- snapskills-backend/User-Service/src/main/java/com/snapskills/userservice/UserServiceApplication.java
+++ snapskills-backend/User-Service/src/main/java/com/snapskills/userservice/UserServiceApplication.java
@@ -2,8 +2,29 @@ package com.snapskills.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SnapSkills User Service API",
+                version = "1.0",
+                description = "This API provides endpoints for managing users within the SnapSkills platform. " +
+                        "It allows for the creation, retrieval, updating, and deletion of user profiles.",
+                contact = @Contact(
+                        name = "SnapSkills Support",
+                        email = "support@snapskills.com",
+                        url = "https://snapskills.com/contact"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        )
+)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
